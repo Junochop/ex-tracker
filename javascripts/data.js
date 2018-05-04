8c099c0d2efb47d7eaa7a685503d62a39cdd1413
Juno Lee <lejuno80@gmail.com>
2018-05-03 23:54:14 -0500

    location

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,4 +1,5 @@
 const loadEx = require('./ex');
+const loadLocation = require('./locations');
 const dom = require('./dom');
 
 const whenExLoad = (data) => {
@@ -11,8 +12,19 @@ const whenExDontLoad = (error) => {
   console.log('error', error);
 };
 
+const whenLocationLoad = (data) => {
+  console.log('data', data);
+  $('#spot').append(dom.writeLocation(data.location));
+
+};
+
+const whenLocationDontLoad = (error) => {
+  console.log('error', error);
+};
+
 const initializer = () => {
   loadEx(whenExLoad, whenExDontLoad);
+  loadLocation(whenLocationLoad, whenLocationDontLoad);
 
 };
 
